c31f340daf16c80837bdc71c89f96a73d24e866b
Mart Aarma <mart.aarma@nortal.com>
2023-11-21 11:24:55 +0200

    MDL-8 Add empty Spring Boot application

---
+++ src/main/java/ee/ria/eudi/qeaa/issuer/IssuerApplication.java
@@ -0,0 +1,13 @@
+package ee.ria.eudi.qeaa.issuer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IssuerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IssuerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ee/ria/eudi/qeaa/issuer/IssuerApplicationTests.java
@@ -0,0 +1,13 @@
+package ee.ria.eudi.qeaa.issuer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IssuerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
