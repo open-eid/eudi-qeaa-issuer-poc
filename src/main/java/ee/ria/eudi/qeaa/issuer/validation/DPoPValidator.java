74d1536484ad1b6135f8e8eae5b4581b778989ff
Mart Aarma <mart.aarma@nortal.com>
2024-09-01 19:19:24 +0300

    MDL-39 Use allowed DPoP algorithms from AS metadata.

--- src/main/java/ee/ria/eudi/qeaa/issuer/validation/DPoPValidator.java
+++ src/main/java/ee/ria/eudi/qeaa/issuer/validation/DPoPValidator.java
@@ -2,23 +2,27 @@ package ee.ria.eudi.qeaa.issuer.validation;
 
 import com.nimbusds.jose.JOSEException;
 import com.nimbusds.jose.JOSEObjectType;
-import com.nimbusds.jose.JWSAlgorithm;
 import com.nimbusds.jose.proc.BadJOSEException;
 import com.nimbusds.jose.proc.DefaultJOSEObjectTypeVerifier;
 import com.nimbusds.jose.proc.SecurityContext;
+import com.nimbusds.jwt.JWTClaimsSet;
 import com.nimbusds.jwt.SignedJWT;
 import com.nimbusds.jwt.proc.ConfigurableJWTProcessor;
 import com.nimbusds.jwt.proc.DefaultJWTProcessor;
 import com.nimbusds.oauth2.sdk.dpop.verifiers.DPoPIssuer;
-import com.nimbusds.oauth2.sdk.dpop.verifiers.DefaultDPoPSingleUseChecker;
+import com.nimbusds.oauth2.sdk.id.JWTID;
+import com.nimbusds.oauth2.sdk.util.singleuse.SingleUseChecker;
 import ee.ria.eudi.qeaa.issuer.configuration.properties.IssuerProperties;
 import ee.ria.eudi.qeaa.issuer.error.ServiceException;
+import ee.ria.eudi.qeaa.issuer.service.AuthorizationServerMetadataService;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Component;
 
+import java.text.ParseException;
 import java.time.Clock;
+import java.util.Map;
 import java.util.Set;
 
 import static ee.ria.eudi.qeaa.issuer.controller.CredentialController.CREDENTIAL_REQUEST_MAPPING;
@@ -27,32 +31,25 @@ import static ee.ria.eudi.qeaa.issuer.error.ErrorCode.INVALID_DPOP_PROOF;
 @Component
 @RequiredArgsConstructor
 public class DPoPValidator {
-    public static final String JOSE_TYPE_DPOP_JWT = "dpop+jwt";
-    private final JwsHeaderKeySelector jwsHeaderKeySelector = new JwsHeaderKeySelector(Set.of(
-        JWSAlgorithm.RS256,
-        JWSAlgorithm.RS384,
-        JWSAlgorithm.RS512,
-        JWSAlgorithm.ES256,
-        JWSAlgorithm.ES384,
-        JWSAlgorithm.ES512,
-        JWSAlgorithm.PS256,
-        JWSAlgorithm.PS384,
-        JWSAlgorithm.PS512
-    ));
+    public static final JOSEObjectType JOSE_TYPE_DPOP_JWT = new JOSEObjectType("dpop+jwt");
     private final IssuerProperties issuerProperties;
-    private final DefaultDPoPSingleUseChecker dPoPSingleUseChecker;
+    private final SingleUseChecker<Map.Entry<DPoPIssuer, JWTID>> dPoPSingleUseChecker;
+    private final AuthorizationServerMetadataService asMetadataService;
     @Getter
     private final Clock systemClock = Clock.systemUTC();
 
-    public void validate(SignedJWT dPoPProof, String clientId, String expectedKeyThumbprint, String expectedAccessTokenHash) {
+    public void validate(SignedJWT dPoPProof, String expectedAccessTokenHash, JWTClaimsSet accessTokenClaims) {
         try {
+            String clientId = accessTokenClaims.getStringClaim("client_id");
+            String expectedKeyThumbprint = (String) accessTokenClaims.getJSONObjectClaim("cnf").get("jkt");
+            JwsHeaderKeySelector jwsHeaderKeySelector = new JwsHeaderKeySelector(Set.copyOf(asMetadataService.getMetadata().getDPoPJWSAlgs()));
             ConfigurableJWTProcessor<SecurityContext> jwtProcessor = new DefaultJWTProcessor<>();
             jwtProcessor.setJWSKeySelector(jwsHeaderKeySelector);
-            jwtProcessor.setJWSTypeVerifier(new DefaultJOSEObjectTypeVerifier<>(new JOSEObjectType(JOSE_TYPE_DPOP_JWT)));
+            jwtProcessor.setJWSTypeVerifier(new DefaultJOSEObjectTypeVerifier<>(JOSE_TYPE_DPOP_JWT));
             jwtProcessor.setJWTClaimsSetVerifier(getClaimsVerifier(clientId, expectedAccessTokenHash));
             jwtProcessor.process(dPoPProof, null);
             validateKeyProof(dPoPProof, expectedKeyThumbprint);
-        } catch (BadJOSEException | JOSEException ex) {
+        } catch (BadJOSEException | JOSEException | ParseException ex) {
             throw new ServiceException(INVALID_DPOP_PROOF, "Invalid DPoP", ex);
         }
     }
